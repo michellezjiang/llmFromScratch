8ddd4e7845501d041d8de4b3ef1f667b5776f2e4
michelle <39465865+michellezjiang@users.noreply.github.com>
2026-09-15 17:38:45 -0400

    adding bytePair

--- bytePair.py
+++ bytePair.py
@@ -1,2 +1,19 @@
 import tiktoken
 tokenizer = tiktoken.get_encoding("gpt2")
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+     "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
+
+unknownWord = "Akwirw ier"
+unknownId = tokenizer.encode(unknownWord, allowed_special={"<|endoftext|>"})
+print(unknownId)
+for token in unknownId:
+    print(tokenizer.decode([token]))
+
+print(tokenizer.decode(unknownId))
